ffd1ca468e207a548aa91fb5969bdf3822f02126
xkhuang <xiaokehuang@foxmail.com>
2020-01-24 23:42:09 +0800

    delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-echo "Changing Blog's Background image..."
-
-sed -e 's/bg[0-9][0-9]/bg00/g' ~/Desktop/blog/themes/next/source/css/_custom/custom.styl > ~/Desktop/blog/tmp__.tmp
-cat ~/Desktop/blog/tmp__.tmp > ~/Desktop/blog/themes/next/source/css/_custom/custom.styl
-rm -rf ~/Desktop/blog/tmp__.tmp
-
-hexo g && hexo d
